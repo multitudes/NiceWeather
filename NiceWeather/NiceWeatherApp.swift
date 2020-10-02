39232552c12a211c33efd8ee9e501fcaa7c2d2de
laurent <wrmultitudes@gmail.com>
2020-10-02 13:35:56 +0200

    Initial Commit

---
+++ NiceWeather/NiceWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  NiceWeatherApp.swift
+//  NiceWeather
+//
+//  Created by Laurent B on 02/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct NiceWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
