1796ebeb79deda5d27c47d8e607798b08fd6af13
laurent <wrmultitudes@gmail.com>
2020-10-06 17:26:25 +0200

    add some tests

--- NiceWeatherTests/NiceWeatherTests.swift
+++ NiceWeatherTests/NiceWeatherTests.swift
@@ -10,8 +10,37 @@ import XCTest
 
 class NiceWeatherTests: XCTestCase {
 
+    var sut: URLSession!
+
+    override func setUp() {
+      super.setUp()
+      sut = URLSession(configuration: .default)
+    }
+
+    override func tearDown() {
+      sut = nil
+      super.tearDown()
+    }
+
+    func testValidCallToAPIGetsHTTPStatusCode200(){
+        let url = URL(string: "http://api.openweathermap.org/data/2.5/weather?q=Berlin,DE&units=metric&APPID=385417c76d45ab1972316b6ffd8b6efa")
+        let promise = expectation(description: "Completion handler invoked")
+        var statusCode: Int?
+        var responseError: Error?
+        let dataTask = sut.dataTask(with: url!) { data, response, error in
+            statusCode = (response as? HTTPURLResponse)?.statusCode
+            responseError = error
+            promise.fulfill()
+        }
+        dataTask.resume()
+        wait(for: [promise], timeout: 5)
+        XCTAssertNil(responseError)
+        XCTAssertEqual(statusCode, 200)
+    }
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        
     }
 
     override func tearDownWithError() throws {
---
+++ NiceWeatherTests/TestModel/TestModel.swift
@@ -0,0 +1,74 @@
+//
+//  TestModel.swift
+//  NiceWeatherTests
+//
+//  Created by Laurent B on 06/10/2020.
+//
+
+import XCTest
+@testable import NiceWeather
+
+class TestModel: XCTestCase {
+    
+    var sut: WeatherModel?
+    
+    override func setUp() {
+      super.setUp()
+      sut = WeatherModel()
+    }
+
+    override func tearDown() {
+      sut = nil
+      super.tearDown()
+    }
+    
+    func testDefaultLocations() {
+        let defaultLocations = WeatherModel.defaultLocations
+        XCTAssertNotNil(defaultLocations.count)
+    }
+    
+    func testLastLocations() {
+        let defaultLastLocation = WeatherModel.defaultLastLocation
+        XCTAssertNotNil(defaultLastLocation)
+    }
+    
+    func testUserDefaultKeys() {
+        XCTAssert(WeatherModel.locationsKey == "Locations", "locationsKey is wrong")
+        XCTAssert(WeatherModel.lastLocationKey == "Lastlocation", "lastLocationKey is wrong")
+    }
+    
+    func testloadLastLocationNotNil() {
+        XCTAssertNotNil(WeatherModel.loadLastLocation(), "loadLastLocation() gives a nil value")
+    }
+
+    func testloadLocations() {
+        XCTAssert(WeatherModel.loadLocations().count > 0, "loadLocations() gives an empty array")
+    }
+    
+    func testcurrentLocationPersistence() {
+        sut?.currentLocation = Location(city: "San Francisco", countryCode: "US")
+        sut?.persistLastLocation()
+        XCTAssert(WeatherModel.loadLastLocation() == sut?.currentLocation , "location does not persist")
+    }
+    
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
