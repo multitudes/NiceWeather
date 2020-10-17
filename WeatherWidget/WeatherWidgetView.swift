aa602f4d7834e5eb071c338fa46577c61f8474de
laurent <wrmultitudes@gmail.com>
2020-10-17 16:29:51 +0200

    playing with widgetview

--- WeatherWidget/WeatherWidgetView.swift
+++ WeatherWidget/WeatherWidgetView.swift
@@ -12,32 +12,108 @@ struct WeatherWidgetData {
 	let city: String
 	let temperature: Measurement<UnitTemperature>
 	let fetchDate: Date
+
+	var photoName: String? = nil
+
 }
 
 
 extension WeatherWidgetData {
-	static let previewData = WeatherWidgetData(city: "Berlin", temperature: Measurement<UnitTemperature>(value: 10, unit: .celsius), fetchDate: Date())
+	static let previewData = WeatherWidgetData(city: "Berlin", temperature: Measurement<UnitTemperature>(value: 10, unit: .celsius), fetchDate: Date(), photoName: "weather")
 }
 
 struct WeatherWidgetView: View {
 	let data: WeatherWidgetData
+	@Environment(\.widgetFamily) var widgetFamily
 
 	var body: some View {
 		ZStack {
 			BackgroundGradient()
+
+			HStack {
+				VStack(alignment: .leading) {
+					WeatherCityView(data: data)
+
+					Spacer()
+
+					TempView(data: data)
+
+
+				}
+				.padding(.all, 10.0)
+				if widgetFamily == .systemMedium,
+					let photoName = data.photoName {
+						Image(photoName)
+							.resizable().padding()
+					}
+			}
+		}
+	}
+}
+
+struct WeatherCityView: View {
+	let data: WeatherWidgetData
+
+	var body: some View {
+		HStack {
 			VStack(alignment: .leading) {
 				Text("City")
 					.font(.body)
 					.foregroundColor(.gray)
 					.bold()
+
+				Spacer()
+
+				Text(data.city)
+					.font(.title)
+					.bold()
+					.foregroundColor(.gray)
+					.minimumScaleFactor(0.8)
 			}
+			.padding(.all, 8.0)
+			Spacer(minLength: 0)
+		}
+		.background(ContainerRelativeShape().fill( Color(.cyan))).opacity(0.8)
+
+	}
+}
+
+struct TempView: View {
+	let data: WeatherWidgetData
+	let measurementFormatter = MeasurementFormatter()
+
+	var body: some View {
+		VStack(alignment: .leading) {
+			Text("Temp: \(measurementFormatter.string(from: data.temperature))")
+				.font(.body)
+				.bold()
+				.foregroundColor(.gray)
+
+			Text("fetched: \(data.fetchDate, style: .relative) ago")
+				.font(.caption)
+				.bold()
+				.foregroundColor(.gray)
+				.minimumScaleFactor(0.8)
 		}
 	}
 }
 
 struct WeatherWidgetView_Previews: PreviewProvider {
 	static var previews: some View {
-		WeatherWidgetView(data: .previewData)
-			.previewContext(WidgetPreviewContext(family: .systemSmall ))
+		Group {
+			WeatherWidgetView(data: .previewData)
+				.previewContext(WidgetPreviewContext(family: .systemSmall ))
+			WeatherWidgetView(data: .previewData)
+				.previewContext(WidgetPreviewContext(family: .systemSmall ))
+				.environment(\.colorScheme, .dark)
+			WeatherWidgetView(data: .previewData)
+				.previewContext(WidgetPreviewContext(family: .systemSmall ))
+				.environment(\.sizeCategory, .extraExtraExtraLarge)
+			WeatherWidgetView(data: .previewData)
+				.previewContext(WidgetPreviewContext(family: .systemSmall ))
+				.redacted(reason: .placeholder)
+			WeatherWidgetView(data: .previewData)
+				.previewContext(WidgetPreviewContext(family: .systemMedium ))
+		}
 	}
 }
